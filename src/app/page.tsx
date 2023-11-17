4134e9034979dcff4fd94434e874ae6d8b8ccd3f
DankAvHi <thexcat86@gmail.com>
2023-11-17 13:06:01 +0300

    [rm] test notes component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { Notes } from "./notes/Notes";
 import styles from "./page.module.css";
 
 export default function Home() {
-    return (
-        <main className={styles.main}>
-            <Notes />
-        </main>
-    );
+    return <main className={styles.main}></main>;
 }
