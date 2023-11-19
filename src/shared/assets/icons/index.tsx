6f73f518c37d3c7df22f1f49d9e0f0018fc577cc
DankAvHi <thexcat86@gmail.com>
2023-11-19 16:07:07 +0300

    [crt] index file for icons

---
+++ src/shared/assets/icons/index.tsx
@@ -0,0 +1,6 @@
+//@ts-nocheck
+import CreateIcon from "./create.svg?component";
+import MenuIcon from "./menu.svg?component";
+import SearchIcon from "./search.svg?component";
+
+export { CreateIcon, MenuIcon, SearchIcon };
