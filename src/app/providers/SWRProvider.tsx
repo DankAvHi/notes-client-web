2da9c481d6bb8afe6120a1bdd7fb49f57d892e52
DankAvHi <thexcat86@gmail.com>
2023-11-19 14:30:18 +0300

    [chg] import order

--- src/app/providers/SWRProvider.tsx
+++ src/app/providers/SWRProvider.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { SWRConfig } from "swr";
-import { fetcher } from "@/shared";
 import { ReactNode } from "react";
+import { SWRConfig } from "swr";
+import { fetcher } from "@/shared/api";
 
 export const SWRProvider = ({ children }: { children: ReactNode }) => {
     return <SWRConfig value={{ fetcher: fetcher }}>{children}</SWRConfig>;
