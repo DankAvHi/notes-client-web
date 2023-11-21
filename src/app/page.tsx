22519b55c6c4c5f2ed1511749a9dbaa0e566c3fe
DankAvHi <thexcat86@gmail.com>
2023-11-21 12:07:42 +0300

    [add] notes list to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Search } from "@/widgets/Search";
 import styles from "./page.module.css";
+import { NotesList } from "@/widgets/NotesList";
 
 export default function Home() {
     return (
         <>
             <main className={styles.main}>
                 <Search />
+                <NotesList />
             </main>
         </>
     );
