cb792d5acbe53a2508c6c8f536dae79abcba9fcd
DankAvHi <thexcat86@gmail.com>
2023-11-21 12:05:57 +0300

    [add] realisation to card component

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -1,3 +1,11 @@
-const Card = () => {
-    return <div></div>;
+import { UIComponentWithChildrenProps } from "../UIComponent";
+import styles from "./Card.module.css";
+
+export const Card = (props: UIComponentWithChildrenProps) => {
+    const { className, children, ...cardProps } = props;
+    return (
+        <div className={`${styles.Card} ${className}`} {...cardProps}>
+            {children}
+        </div>
+    );
 };
