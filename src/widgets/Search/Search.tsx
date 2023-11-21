bff0b4549109d1ae45ec73491dfed4351c949a10
DankAvHi <thexcat86@gmail.com>
2023-11-21 12:07:17 +0300

    [chg] button element to IconButton component

--- src/widgets/Search/Search.tsx
+++ src/widgets/Search/Search.tsx
@@ -2,13 +2,14 @@ import { MenuIcon, SearchIcon } from "@/shared/assets";
 import iconStyles from "@/shared/styles/global/icons.module.css";
 import materialTypographyStyles from "@/shared/styles/themes/material/typography.module.css";
 import styles from "./Search.module.css";
+import { IconButton } from "@/shared/ui";
 
 export const Search = () => {
     return (
         <div className={styles.Search}>
-            <button className={styles.menuButton} title="Меню" type="button">
+            <IconButton title="Меню" type="button">
                 <MenuIcon className={iconStyles.default} />
-            </button>
+            </IconButton>
             <input
                 className={`${materialTypographyStyles["body-large"]} ${styles.input}`}
                 type="text"
