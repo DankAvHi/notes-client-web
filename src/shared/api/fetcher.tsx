be129bb8420ef3f01ea6ffe7fe9409cedf121aa3
DankAvHi <thexcat86@gmail.com>
2023-11-21 12:04:22 +0300

    [add] eslint ignore comment

--- src/shared/api/fetcher.tsx
+++ src/shared/api/fetcher.tsx
@@ -1,4 +1,5 @@
-type ArgumentTypes<F extends Function> = F extends (...args: infer A) => any ? A : never;
+// eslint-disable-next-line @typescript-eslint/ban-types
+type ArgumentTypes<F extends Function> = F extends (...args: infer A) => unknown ? A : never;
 
 export const fetcher = async (...args: ArgumentTypes<typeof fetch>) => {
     const url = `http://localhost:3000/${args[0]}`;
