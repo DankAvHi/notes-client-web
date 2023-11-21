6dc87d70dfa04e5a5d7a50bce0432913689cd14e
DankAvHi <thexcat86@gmail.com>
2023-11-21 14:00:24 +0300

    [chg] moked data to database

--- src/widgets/NotesList/NotesList.tsx
+++ src/widgets/NotesList/NotesList.tsx
@@ -1,48 +1,27 @@
+"use client";
+
 import { Card } from "@/shared/ui";
 import styles from "./NotesList.module.css";
 import typographyStyles from "@/shared/styles/themes/material/typography.module.css";
-import type { Notes } from "@/shared/api";
-
-const notesMock: Notes = [
-    {
-        id: 0,
-        content: `Длинный текст очень важной, или не очень, заметки. Текст рыба не работает, доказано`,
-        title: "Заголовок",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    },
-    {
-        id: 1,
-        content: `Длинный текст очень важной, или не очень, заметки. Текст рыба не работает, доказано`,
-        title: "Заголовок",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    },
-    {
-        id: 2,
-        content: `Длинный текст очень важной, или не очень, заметки. Текст рыба не работает, доказано`,
-        title: "Заголовок",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    },
-    {
-        id: 3,
-        content: `Длинный текст очень важной, или не очень, заметки. Текст рыба не работает, доказано`,
-        title: "Заголовок",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    },
-];
+import { useGetNotes } from "@/entities/note";
 
 export const NotesList = () => {
+    const { data, error, isLoading } = useGetNotes();
+
+    if (error) {
+        return <p>{`Ошибка при загрузке данных`}</p>;
+    }
+
     return (
         <div className={styles.NotesList}>
-            {notesMock.map((note) => (
-                <Card className={styles.Note} key={note.id}>
-                    <h3 className={typographyStyles["title-medium"]}>{note.title}</h3>
-                    <p className={typographyStyles["body-medium"]}>{note.content}</p>
-                </Card>
-            ))}
+            {isLoading
+                ? `Загрузка`
+                : data.map((note) => (
+                      <Card className={styles.Note} key={note.id}>
+                          <h3 className={typographyStyles["title-medium"]}>{note.title}</h3>
+                          <p className={typographyStyles["body-medium"]}>{note.content}</p>
+                      </Card>
+                  ))}
         </div>
     );
 };
