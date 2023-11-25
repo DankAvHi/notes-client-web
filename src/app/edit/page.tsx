7152a95b0185637e562b877ea8deafa3c084d3cf
DankAvHi <thexcat86@gmail.com>
2023-11-25 18:18:41 +0300

    [add] TopBar to edit component

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,9 +1,12 @@
+import { TopBar } from "./TopBar";
 import styles from "./page.module.css";
 
 export default function Edit() {
     return (
         <>
-            <main className={styles.edit}></main>
+            <main className={styles.edit}>
+                <TopBar />
+            </main>
         </>
     );
 }
