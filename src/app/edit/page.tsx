e091f96897b1becea2ba75c506e2686a94b6d43b
DankAvHi <thexcat86@gmail.com>
2023-11-27 19:17:07 +0300

    [add] editor to edit page

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,12 +1,15 @@
+import { Editor } from "./Editor";
 import { TopBar } from "./TopBar";
+import { EditProvider } from "./edit.provider";
 import styles from "./page.module.css";
 
 export default function Edit() {
     return (
-        <>
+        <EditProvider>
             <main className={styles.edit}>
                 <TopBar />
+                <Editor />
             </main>
-        </>
+        </EditProvider>
     );
 }
