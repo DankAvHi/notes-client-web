93021e0d683444dd1a4d531d7a1ba16cc89af5d4
DankAvHi <thexcat86@gmail.com>
2023-11-27 19:20:55 +0300

    [chg] create new note component

--- src/widgets/CreateNewNote/CreateNewNote.tsx
+++ src/widgets/CreateNewNote/CreateNewNote.tsx
@@ -1,13 +1,14 @@
 import { FAB } from "@/shared/ui";
 import styles from "./CreateNewNote.module.css";
 import typographyStyles from "@/shared/styles/themes/material/typography.module.css";
+import iconStyles from "@/shared/styles/global/icons.module.css";
 import { CreateIcon } from "@/shared/assets";
 
 export const CreateNewNote = () => {
     return (
-        <FAB elementType="link" href={`new`} className={styles.CreateNewNote}>
-            <CreateIcon />
-            <p className={typographyStyles["label-large"]}>{`Создать`}</p>
+        <FAB elementType="link" href={`edit`} className={styles.CreateNewNote}>
+            <CreateIcon className={`${iconStyles.small}`} />
+            <p className={`${typographyStyles["label-large"]}`}>{`Создать`}</p>
         </FAB>
     );
 };
