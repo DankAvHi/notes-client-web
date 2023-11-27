37fc14741495379c6d8a49cb09042d5a89a633c5
DankAvHi <thexcat86@gmail.com>
2023-11-27 19:13:48 +0300

    [rm] uneeded class names

--- src/widgets/Search/Search.tsx
+++ src/widgets/Search/Search.tsx
@@ -1,5 +1,4 @@
 import { MenuIcon, SearchIcon } from "@/shared/assets";
-import iconStyles from "@/shared/styles/global/icons.module.css";
 import materialTypographyStyles from "@/shared/styles/themes/material/typography.module.css";
 import styles from "./Search.module.css";
 import { IconButton } from "@/shared/ui";
@@ -8,7 +7,7 @@ export const Search = () => {
     return (
         <div className={styles.Search}>
             <IconButton title="Меню" type="button">
-                <MenuIcon className={iconStyles.default} />
+                <MenuIcon />
             </IconButton>
             <input
                 className={`${materialTypographyStyles["body-large"]} ${styles.input}`}
@@ -18,7 +17,7 @@ export const Search = () => {
                 title="Поиск"
                 placeholder="Поиск заметок..."
             />
-            <SearchIcon className={iconStyles.defaultAsymetry} />
+            <SearchIcon />
         </div>
     );
 };
