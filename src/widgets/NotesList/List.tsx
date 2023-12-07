e51aec1613df4c205457b68ea1cfd2c6075fbb1e
DankAvHi <thexcat86@gmail.com>
2023-12-07 16:20:45 +0300

    [add] pined and deleted filter to list

--- src/widgets/NotesList/List.tsx
+++ src/widgets/NotesList/List.tsx
@@ -5,10 +5,19 @@ import { NoteCard } from "@/entities/note";
 
 export const List = (props: { notes: Notes }) => {
     const { notes } = props;
+    const pinnedNotes = notes.filter((note) => note.pinned && !note.deleted);
+    const defaultNotes = notes.filter((note) => !note.pinned && !note.deleted);
     return (
         <>
             {notes.length > 0 ? (
-                notes.map((note) => <NoteCard note={note} key={`${note.id} ${note.createdAt}`} />)
+                <>
+                    {pinnedNotes.map((note) => (
+                        <NoteCard note={note} key={`${note.id} ${note.createdAt}`} />
+                    ))}
+                    {defaultNotes.map((note) => (
+                        <NoteCard note={note} key={`${note.id} ${note.createdAt}`} />
+                    ))}
+                </>
             ) : (
                 <p>{`Нет сохранённых заметок`}</p>
             )}
