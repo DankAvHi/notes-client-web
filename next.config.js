d92e63ef81e05994e8fbc13e091b85854ec1f867
DankAvHi <thexcat86@gmail.com>
2023-12-18 15:41:06 +0300

    [add] disabling pwa in dev mode

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
+    disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig = withPWA({
